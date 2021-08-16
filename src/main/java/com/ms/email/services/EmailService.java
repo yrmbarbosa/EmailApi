6d0eac5fd8b194f6baad7b06ec636dbd8e48ba8c
Yuri Barbosa <yrmbarbosa@gmail.com>
2021-08-16 14:38:46 -0300

    Publicando o projeto

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,44 @@
+package com.ms.email.services;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.Email;
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage emailMessage = new SimpleMailMessage();
+
+            emailMessage.setFrom(emailModel.getEmailFrom());
+            emailMessage.setTo(emailModel.getEmailTo());
+            emailMessage.setSubject(emailModel.getSubject());
+            emailMessage.setText(emailModel.getText());
+            emailSender.send(emailMessage);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException ex) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+
+    }
+}
