f903c5bb883b64ced127175a27f4fca3bc9cae61
Yuri Barbosa <yrmbarbosa@gmail.com>
2021-08-17 11:18:57 -0300

    Alteração da model de Email de Id para UUID

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -9,7 +9,6 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import javax.validation.constraints.Email;
 import java.time.LocalDateTime;
 
 @Service
